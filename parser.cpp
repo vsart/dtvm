277690fd4e1a719e404fd48f8d8e8f158a3a6e7a
vsart <vct.sartor@gmail.com>
2017-02-06 13:09:21 -0200

    Commented parser

--- parser.cpp
+++ parser.cpp
@@ -9,6 +9,13 @@
 #include "error.hpp"
 
 
+// get_int
+// Tries to get an integer from the stringstream and prints out an error on failure.
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @ret - Second value reports true if there was an error and the first on is the integer
+//        value if the second value is false.
 std::tuple<int64_t, bool> get_int(std::stringstream &ss, const std::string &sn, const int &ln) {
 	int64_t integer_token;
 	ss >> integer_token;
@@ -20,8 +27,16 @@ std::tuple<int64_t, bool> get_int(std::stringstream &ss, const std::string &sn, 
 }
 
 
+// get_reg
+// Tries to get an integer from the stringstream that represents a register index, and prints
+// out an error on failure.
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @ret - Second value reports true if there was an error and the first on is the index
+//        number if the second value is false.
 std::tuple<int64_t, bool> get_reg(std::stringstream &ss, const std::string &sn, const int &ln) {
-	// @TODO Check if integer value is actually valid
+	// @TODO Check if integer value is actually within range
 	int64_t integer_token;
 	ss >> integer_token;
 	if (ss.fail()) {
@@ -32,6 +47,14 @@ std::tuple<int64_t, bool> get_reg(std::stringstream &ss, const std::string &sn, 
 }
 
 
+// get_float
+// Tries to get a floating number from the stringstream that represents a register index, and
+// prints out an error on failure.
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @ret - Second value reports true if there was an error and the first on is the floating
+//        point number if the second value is false.
 std::tuple<double, bool> get_float(std::stringstream &ss, const std::string &sn, const int &ln) {
 	double float_token;
 	ss >> float_token;
@@ -43,6 +66,12 @@ std::tuple<double, bool> get_float(std::stringstream &ss, const std::string &sn,
 }
 
 
+// check_empty
+// Checks if the stringstream is semantically empty and prints an error otherwise.
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @ret - Returns true if the stringstream is not semantically empty.
 bool check_empty(std::stringstream &ss, const std::string &sn, const int &ln)
 {
 	std::string token;
@@ -59,6 +88,13 @@ bool check_empty(std::stringstream &ss, const std::string &sn, const int &ln)
 }
 
 
+// parse_reg_reg
+// Modular parsing block to fetch two register tokens from the stringstream
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @arg c  - Code object to push the parsed values into
+// @ret - Returns true if there was an error.
 bool parse_reg_reg(std::stringstream &ss, const std::string &sn, const int &ln, Code &c)
 {
 	auto tmp1 = get_reg(ss, sn, ln);
@@ -78,6 +114,13 @@ bool parse_reg_reg(std::stringstream &ss, const std::string &sn, const int &ln, 
 }
 
 
+// parse_int_reg
+// Modular parsing block to fetch an integer and a register token from the stringstream
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @arg c  - Code object to push the parsed values into
+// @ret - Returns true if there was an error.
 bool parse_int_reg(std::stringstream &ss, const std::string &sn, const int &ln, Code &c)
 {
 	auto tmp1 = get_int(ss, sn, ln);
@@ -97,6 +140,13 @@ bool parse_int_reg(std::stringstream &ss, const std::string &sn, const int &ln, 
 }
 
 
+// parse_flt_reg
+// Modular parsing block to fetch a floating point and a register token from the stringstream
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @arg c  - Code object to push the parsed values into
+// @ret - Returns true if there was an error.
 bool parse_flt_reg(std::stringstream &ss, const std::string &sn, const int &ln, Code &c)
 {
 	auto tmp1 = get_float(ss, sn, ln);
@@ -116,6 +166,13 @@ bool parse_flt_reg(std::stringstream &ss, const std::string &sn, const int &ln, 
 }
 
 
+// parse_int_reg
+// Modular parsing block to fetch a register token from the stringstream
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @arg c  - Code object to push the parsed values into
+// @ret - Returns true if there was an error.
 bool parse_reg(std::stringstream &ss, const std::string &sn, const int &ln, Code &c)
 {
 	auto tmp1 = get_reg(ss, sn, ln);
@@ -130,7 +187,16 @@ bool parse_reg(std::stringstream &ss, const std::string &sn, const int &ln, Code
 }
 
 
-bool parse_lab(std::stringstream &ss ,const std::string &sn, const int &ln, Code &c, std::map<int64_t, std::string> &m)
+// parse_lab
+// Modular parsing block to fetch a label token from the stringstream
+// @arg ss - stringstream which the token must originate from
+// @arg sn - File name for error reporting
+// @arg ln - Line number for error reporting
+// @arg c  - Code object to push the parsed values into
+// @arg m  - Map to store the information regarding the label reference
+// @ret - Returns true if there was an error.
+bool parse_lab(std::stringstream &ss ,const std::string &sn, const int &ln, Code &c,
+			   std::map<int64_t, std::string> &m)
 {
 	std::string token;
 	ss >> token;
@@ -145,6 +211,12 @@ bool parse_lab(std::stringstream &ss ,const std::string &sn, const int &ln, Code
 }
 
 
+// parse
+// @exported
+// Parses the source file into a Code object
+// @arg src      - The ifstream holding the source file
+// @arg src_name - The name of the source file for error reporting
+// @ret - The Code object. An empty Code object is returned on error.
 Code parse(std::ifstream& src, std::string& src_name)
 {
 	// Rely that src is opened and at the proper point
@@ -153,7 +225,9 @@ Code parse(std::ifstream& src, std::string& src_name)
 	int line_num = 0;
 	std::string line;
 
+	// Holds the strings of the labels and the index they reference
 	std::map<std::string, int64_t> label_dict;
+	// Holds the index label references appear and the name of the label referenced
 	std::map<int64_t, std::string> label_refs;
 
 	while (std::getline(src, line)) {
@@ -168,16 +242,19 @@ Code parse(std::ifstream& src, std::string& src_name)
 		line_stream >> token;
 
 		// Check if line is a comment
+		// The other possibility of a comment is handled by `check_empty`
 		if (token[0] == ';')
 			continue;
 
 		// Check if line is a label
 		if (token[0] == '@') {
-			// @TODO Check if label is already defined and don't allow
-			// substitutions.
+			// @TODO Check if label is already defined and don't allow substitutions.
 			label_dict[token] = code.curr_index;
 			continue;
 		}
+
+
+		// Try to match an instruction and error otherwisde
 
 		if (token == "halt") {
 			code.push_op(op::halt);
@@ -286,16 +363,20 @@ Code parse(std::ifstream& src, std::string& src_name)
 		}
 	}
 
-	// Push a halt at the end
+	// Always make sure to push a halt at the end
+	// 1. This avoids empty Code object when an empty source is given
+	// 2. This makes it so a label at the end of a file points to something meaningful
 	code.push_op(op::halt);
 
+	// Go thorugh label references and use the label dictionary to substitute the proper
+	// 'addresses' where they were referenced.
 	for (auto &ref : label_refs) {
 		const auto index_to_change = ref.first;
 		const auto referenced_label = ref.second;
 		const auto find_result = label_dict.find(referenced_label);
 		if (find_result == label_dict.end()) {
 			// @TODO Add information about where this label was used
-			std::cerr << Error() << "Unknown label " << referenced_label << std::endl;
+			std::cerr << Error() << "Unknown label '" << referenced_label << '\'' << std::endl;
 			return Code();
 		}
 		const auto referenced_index = find_result->second;
--- parser.hpp
+++ parser.hpp
@@ -6,4 +6,10 @@
 #include "vm_types.hpp"
 
 
+// parse
+// @exported
+// Parses the source file into a Code object
+// @arg src      - The ifstream holding the source file
+// @arg src_name - The name of the source file for error reporting
+// @ret - The Code object. An empty Code object is returned on error.
 Code parse(std::ifstream&, std::string&);
